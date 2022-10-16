62b12b1fd6b362eb2c0881d88fd8825e600e767e
Henrique Sebastião <henriquesebastiao.web@gmail.com>
2022-10-16 10:38:05 -0400

    Código exercício 8

---
+++ Python/Curso em Vídeo/Mundo 1/exercicio8.py
@@ -0,0 +1,8 @@
+'''
+Escreva um programa que leia um valor em metros e o exiba convertido em centímetro s e milímetros.
+'''
+
+metros = float(input('Digite um valor em metros: '))
+centimetros = metros * 100
+milimilímetros = metros * 1000
+print(f'O valor de {metros} metros convertido em centímetros é {centimetros} e em milímetros é {milimilímetros}.')
