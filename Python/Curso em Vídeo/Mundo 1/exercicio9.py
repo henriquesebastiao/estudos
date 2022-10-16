b89de6ed82eb13e619a2934ebdf801477976334c
Henrique Sebastião <henriquesebastiao.web@gmail.com>
2022-10-16 11:02:02 -0400

    Correção do comentário.

--- Python/Curso em Vídeo/Mundo 1/exercicio9.py
+++ Python/Curso em Vídeo/Mundo 1/exercicio9.py
@@ -1,3 +1,9 @@
 '''
-Fça um prgrama que leia um número inteiro 
+Faça um prgrama que leia um número inteiro qualquer e mostre na tela a sua tabuada.
 '''
+
+num = int(input('Digite um número: '))
+cont = 1
+while cont <= 10:
+    print(f'{num} x {cont} = {num * cont}')
+    cont += 1
