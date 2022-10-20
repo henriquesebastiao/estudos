3e34874b565752ad80345d4a0418bff201be9736
Henrique Sebastião <henriquesebastiao.web@gmail.com>
2022-10-19 21:12:01 -0400

    Adicionando conteúdo da aula de cores no terminal

---
+++ Curso em Vídeo/Python/Mundo 1/coresNoTerminal.py
@@ -0,0 +1,8 @@
+# Cores no terminal
+
+print("\033[0;30;41mOlá Mundo!")
+print("\033[4;33;44mOlá Mundo!")
+print("\033[1;35;43mOlá Mundo!")
+print("\033[30;42mOlá Mundo!")
+print("\033[mOlá Mundo!")
+print("\033[7;30mOlá Mundo!")
