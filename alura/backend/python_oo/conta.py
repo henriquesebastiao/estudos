50bf6ccf2b77724de242adf57efdc418707c3487
Henrique Sebastião <henriquesebastiao.web@gmail.com>
2022-12-11 21:48:55 -0400

    *

--- alura/backend/python_oo/conta.py
+++ alura/backend/python_oo/conta.py
@@ -1,18 +1,24 @@
 class Conta:
     # A função __init__ é um método especial que é chamado (altomaticamente) quando a classe é instanciada
     def __init__(self, numero, titular, saldo, limite):
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        # Colocar o __ antes do nome da variável torna ela privada
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self):
-        print(f"Saldo de {self.saldo} do titular {self.titular}")
+        print(f"Saldo de {self.__saldo} do titular {self.__titular}")
 
     def saca(self, valor):
-        self.saldo -= valor
-        print(f"Saldo de {self.saldo} do titular {self.titular}")
+        self.__saldo -= valor
+        print(f"Saldo de {self.__saldo} do titular {self.__titular}")
 
     def deposita(self, valor):
-        self.saldo += valor
-        print(f"Saldo de {self.saldo} do titular {self.titular}")
+        self.__saldo += valor
+        print(f"Saldo de {self.__saldo} do titular {self.__titular}")
+
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
+        print(f"Saldo de {self.__saldo} do titular {self.__titular}")
